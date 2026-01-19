5e970f1cd671ceaece2afbdbb2422b30e87e2ef8
Praveen Sharma <praying.sharma@gmail.com>
2026-01-19 10:27:34 +0530

    adding-schema

---
+++ backend/models/product.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+
+    category: {
+        type: String,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true,
+        min: 0,
+        default: 0
+    },
+    images: [{
+        type: String,
+        required: true
+    }],
+    averageRating: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 5,
+        default: 0
+    },
+    totalReviews: {
+        type: Number,
+        default: 0
+    }
+
+}, { timestamps: true })
+
+
+export const Product = mongoose.model("Product", productSchema);
